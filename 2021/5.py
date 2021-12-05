20320e9adc58fb561deb91af46932af1ec2df06d
Max Meinhold <mxmeinhold@gmail.com>
2021-12-05 01:44:04 -0500

    2021 Day 5

---
+++ 2021/5.py
@@ -0,0 +1,94 @@
+#! /bin/python3
+
+import sys
+from itertools import chain
+
+class Coord:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other):
+        if not isinstance(other, Coord):
+            return False
+        return self.x == other.x and self.y == other.y
+
+    def __hash__(self):
+        return (self.x, self.y).__hash__()
+
+    def __str__(self):
+        return f'Coord({self.x}, {self.y})'
+
+    def __repr__(self):
+        return str(self)
+
+
+class VentLine:
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return f'VentLine({self.left}, {self.right})'
+
+    def __init__(self, line):
+        left, right = tuple(line.split(' -> '))
+        self.left = Coord(*map(int, left.split(',')))
+        self.right = Coord(*map(int, right.split(',')))
+
+        self.coords = set()
+        x_diff = 1 if self.left.x < self.right.x else (0 if self.left.x == self.right.x else -1)
+        y_diff = 1 if self.left.y < self.right.y else (0 if self.left.y == self.right.y else -1)
+        c = self.left
+        while c != self.right:
+            self.coords.add(c)
+            c = Coord(c.x+x_diff, c.y+y_diff)
+        self.coords.add(self.right)
+
+    def is_vertical(self) -> bool:
+        return self.left.x == self.right.x
+
+    def is_horizantal(self) -> bool:
+        return self.left.y == self.right.y
+
+    def overlap(self, other) -> set[Coord]:
+        return self.coords & other.coords
+
+class Map:
+    def __init__(self, vents):
+        self.vents = list(vents)
+
+        self.coords = list(chain.from_iterable(map(lambda v: v.coords, self.vents)))
+
+        self.width = max(map(lambda c: c.x, self.coords))+1
+        self.height = max(map(lambda c: c.y, self.coords))+1
+
+    def num_overlaps(self):
+        return sum(map(lambda i: i>1, chain.from_iterable(self.map())))
+
+    def map(self):
+        m = [
+            [0] * self.width
+            for _ in range(self.height)
+        ]
+
+        for c in self.coords:
+            m[c.y][c.x] +=1
+
+        return m
+
+    def __repr__(self):
+        return '\n'.join(map(lambda l: ''.join(map(str, l)), (self.map()))).replace('0','.')
+
+
+with open(sys.argv[1], 'r') as in_file:
+    vents = list(map(VentLine, map(str.strip, in_file)))
+
+### Part 1 ###
+# only vertical or horizantal lines
+part1 = Map(filter(lambda v: v.is_vertical() or v.is_horizantal(), vents))
+print(f'Part 1: {part1.num_overlaps()}')
+
+### Part 2 ###
+part2 = Map(vents)
+print(f'Part 2: {part2.num_overlaps()}')
