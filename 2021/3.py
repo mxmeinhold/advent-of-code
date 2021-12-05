cd98a9145b019bb0e6cef75df8e7ed309ebd17cd
Max Meinhold <mxmeinhold@gmail.com>
2021-12-04 23:36:35 -0500

    2021 day 3, cleaned up
    Now with typing, and docstrings

--- 2021/3.py
+++ 2021/3.py
@@ -1,66 +1,84 @@
 #!/bin/python3
 
 import sys
+from functools import reduce
+from typing import Literal, Optional, cast, TypeVar
+from collections.abc import Iterator
+
+Bit = Literal['0', '1']
+
 
 with open(sys.argv[1], 'r') as in_file:
-    data = list(in_file)
+    tups = cast(
+        list[tuple[Bit, ...]],
+        list(map(tuple, map(str.strip, in_file))),
+    )
+
+def to_int(t: tuple[Bit, ...]) -> int:
+    """
+    Take a tuple of '0's and '1's, and convert it to an int by interpreting it
+    as a binary string
+    """
+    return int(''.join(t), base=2)
+
+def count(col: tuple[Bit, ...], prefer: Optional[Bit] = None) -> tuple[Bit, Bit]:
+    """
+    Take a tuple of Bits, and return (<most common>, <least common>)
+
+    if there are an equal number of ones and zeros:
+        if prefer is set, returns (prefer, prefer)
+        otherwise, raises Exception
+    """
+    def bump(l: list[int], n: Bit) -> list[int]:
+        l[int(n)] += 1
+        return l
+
+    # count how many zeros and ones there are
+    zeros, ones = reduce(bump, col, [0,0])
+
+    # return most, least
+    if zeros > ones:
+        return '0', '1'
+    if ones > zeros:
+        return '1', '0'
+    if prefer:
+        return prefer, prefer
+    raise Exception('equal 1s and 0s, and no prefer specified')
 
 ### Part 1
-tups = []
-for line in data:
-    tups.append(tuple(line.strip()))
-print(tups[0])
 
-def count(col):
-    d = dict()
-    for c in col:
-        d[c] = 1 + d.get(c, 0)
-    if max(d.values()) == min(d.values()):
-        raise
-    return max(d, key= d.get), min(d, key= d.get)
-
-gamma_str = []
-epsilon_str = []
-cols = list(zip(*tups))
-for col in cols:
-    g, e = count(col)
-    gamma_str.append(g)
-    epsilon_str.append(e)
-print(gamma_str)
-
-
-gamma_rate = int(''.join(gamma_str), base=2)# bit 1 = mode of bits 1 in the other numbers, etc.
-epsilon_rate = int(''.join(epsilon_str), base=2)# bit 1 = mode of bits 1 in the other numbers, etc.
-print(gamma_rate)
-print(epsilon_rate)
-
-print(f'Part 1: {gamma_rate * epsilon_rate}')
+# The following line, explained from outside -> in
+# reduce(int.__mul__): reduce the mapping to an integer by multiplication
+# (tl;dr: this does the gamm_rate * epsilon_rate)
+#  map(to_int): converts every row into a string of binary digits, then to an int
+#   zip: gives us an iterator of rows
+#    map(count): gets the most common digit of each column
+#     zip: gets us an iterator of tuples
+print(f'Part 1: {reduce(int.__mul__, map(to_int, zip(*map(count, zip(*tups)))))}')
 
 ### Part 2
-ans = 0
 
-ogr = list(tups)
-csr = list(tups)
+ogr = tups.copy()
+csr = tups.copy()
 idx = 0
-while len(ogr) > 1:
-    o_col = list(zip(*ogr))[idx]
-    try:
-        o, _ = count(o_col)
-    except:
-        o = '1'
-    ogr = list(filter(lambda t: t[idx] == o, ogr))
-    idx += 1
-idx = 0
-while len(csr) > 1:
-    c_col = list(zip(*csr))[idx]
-    try:
-        _, c = count(c_col)
-    except:
-        c = '0'
-    csr = list(filter(lambda t: t[idx] == c, csr))
-    idx += 1
 
-ogr = int(''.join(*ogr), base=2)
-csr = int(''.join(*csr), base=2)
+T = TypeVar('T')
+def index_iter(it: Iterator[T], index: int) -> T:
+    for _ in range(index):
+        _ = next(it)
+    return next(it)
 
-print(f'Part 2: {ogr * csr}')
+def part2(ts: list[tuple[Bit, ...]], prefer: Bit) -> tuple[Bit, ...]:
+    index = 0
+    while len(ts) > 1:
+        col = index_iter(zip(*ts), index)
+        o = count(col, prefer=prefer)[~int(prefer)]
+        ts = list(filter(lambda t: t[index] == o, ts))
+        index += 1
+    return ts[0]
+
+oxygen = to_int(part2(ogr, '1'))
+co2 = to_int(part2(csr, '0'))
+
+
+print(f'Part 2: {oxygen * co2}')
