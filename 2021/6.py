ea4aa92e7658fff6c12bea1718eae48119a01918
Max Meinhold <mxmeinhold@gmail.com>
2021-12-06 00:42:35 -0500

    2021 Day 6 part 2

--- 2021/6.py
+++ 2021/6.py
@@ -7,19 +7,22 @@ with open(sys.argv[1], 'r') as in_file:
     # a lanternfish in this list is represented by it's timer
     lanternfish = list(map(int, next(in_file).split(',')))
 
-### Part 1 ###
+@lru_cache
+def predict(timer: int, days: int) -> int:
+    """
+    Get the number of lanternfish there will be after days days if this is the
+    only lantern fish, and it's timer is timer.
 
-def handle():
-    """ Apply a day to the list """
-    new_lanternfish = []
-    for i in range(len(lanternfish)):
-        lanternfish[i] -= 1
-        if lanternfish[i] == -1:
-            lanternfish[i] = 6
-            new_lanternfish.append(8)
-    lanternfish.extend(new_lanternfish)
+    Since there's only 9 possible timer values for each possible day, we gain a
+    lot by doing memoization as we recurse and the tree expands.
+    """
+    if days == 0:
+        return 1
 
-for i in range(80):
-    handle()
+    if timer == 0:
+        return predict(6, days-1) + predict(8, days-1)
 
-print(f'Part 1: {len(lanternfish)}')
+    return predict(timer-1, days-1)
+
+print('Part 1: ', sum(map(lambda timer: predict(timer, 80), lanternfish)))
+print('Part 2: ', sum(map(lambda timer: predict(timer, 256), lanternfish)))
