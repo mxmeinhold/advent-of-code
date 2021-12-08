cf8a316634fe9d7ddb3db8ce0284ac61c5712f26
Max Meinhold <mxmeinhold@gmail.com>
2021-12-08 02:56:46 -0500

    [2021][8] Cleaned up

--- 2021/8.py
+++ 2021/8.py
@@ -2,106 +2,99 @@
 
 import sys
 
-with open(sys.argv[1], 'r') as in_file:
-    lines = []
-    for line in in_file:
-        patterns, output = tuple(line.split('|'))
-        patterns = patterns.strip().split()
-        outputs = output.strip().split()
-        lines.append((patterns, outputs))
+# Count the number of signals that are 1, 4, 7, or 8
+# (the ones we know based on length alone)
+part1 = 0
 
-nums = {
+# Sum the outputs
+part2 = 0
+
+len_to_num = {
     2: 1,
     3: 7,
     4: 4,
-    5: [2, 3, 5],
-    6: [0, 6, 9],
     7: 8,
 }
 
 proper = {
-    0: set('abcefg'),
-    1: set('cf'),
-    2: set('acdeg'),
-    3: set('acdfg'),
-    4: set('bcdf'),
-    5: set('abdfg'),
-    6: set('abdefg'),
-    7: set('acf'),
-    8: set('abcdefg'),
-    9: set('abcdfg'),
+    'abcefg'  : '0',
+    'cf'      : '1',
+    'acdeg'   : '2',
+    'acdfg'   : '3',
+    'bcdf'    : '4',
+    'abdfg'   : '5',
+    'abdefg'  : '6',
+    'acf'     : '7',
+    'abcdefg' : '8',
+    'abcdfg'  : '9',
 }
 
-proper_inv = {''.join(sorted(proper[n])): n for n in proper}
+with open(sys.argv[1], 'r') as in_file:
+    for line in in_file:
+        signals, output = tuple(line.split('|'))
 
-final_outs = []
-full_outs =[]
+        # { number: signal }
+        ans = {}
+        
+        # all signals of length 5
+        fives = []
 
-for patterns, outputs in lines:
-    ans = {}
-    fives = []
-    sixes = []
-    for pat in patterns:
-        if len(pat) == 5:
-            fives.append(set(pat))
-        elif len(pat) == 6:
-            sixes.append(set(pat))
-        else:
-            ans[nums[len(pat)]] = set(pat)
+        # all signals of length 6
+        sixes = []
 
-    candidates = { chr(c): set(map(chr, range(ord('a'), ord('g') + 1))) for c in range(ord('a'), ord('g')+1) }
+        for pat in map(set, signals.strip().split()):
+            if len(pat) == 5:
+                fives.append(pat)
+            elif len(pat) == 6:
+                sixes.append(pat)
+            else:
+                ans[len_to_num[len(pat)]] = pat
 
-    #for num in ans:
-        #for c in proper[num]:
-            #candidates[c] &= set(ans[num])
+        candidates = { c: set('abcdefg') for c in 'cef' }
 
-    # we know 1, 7, 4, 8
-    # a = 7-1
-    candidates['a'] = ans[7] - ans[1]
-    for c in set(candidates) - {'a'}:
-        candidates[c] -= candidates['a']
+        # we know 1, 7, 4, 8
+        # a = 7-1
+        candidates['a'] = ans[7] - ans[1]
 
+        # b,d = 4-1
+        candidates['b'] = ans[4] - ans[1]
+        candidates['d'] = ans[4] - ans[1]
 
-    # b,d = 4-1
-    candidates['b'] = ans[4] - ans[1]
-    candidates['d'] = ans[4] - ans[1]
-    for c in set(candidates) - {'b', 'd'}:
-       candidates[c] -= candidates['b'] & candidates['d']
+        # e,g = 8-7-4
+        candidates['e'] = ans[8]-ans[7]-ans[4]
+        candidates['g'] = ans[8]-ans[7]-ans[4]
 
-    # e,g = 8-7-4
-    candidates['e'] = ans[8]-ans[7]-ans[4]
-    candidates['g'] = ans[8]-ans[7]-ans[4]
-    for c in set(candidates) - {'e', 'g'}:
-       candidates[c] -= candidates['e'] & candidates['g']
+        for c in 'cf':
+            for oc in 'abdeg':
+                candidates[c] -= candidates[oc]
 
-    # {'g', 'd', 'a'} in all of fives
-    # {'b', 'g', 'a', 'f'}  in all of sixes
-    fs = set(map(chr, range(ord('a'), ord('g') + 1)))
-    for f in fives:
-        fs &= f
-    for c in {'g', 'd', 'a'}:
-        candidates[c] &= fs
+        # {'g', 'd', 'a'} in all of fives
+        fs = set.intersection(*fives)
+        for c in {'g', 'd', 'a'}:
+            candidates[c] &= fs
 
-    ss = set(map(chr, range(ord('a'), ord('g') + 1)))
-    for s in sixes:
-        ss &= s
-    for c in {'b', 'g', 'a', 'f'}:
-        candidates[c] &= ss
+        # {'b', 'g', 'a', 'f'}  in all of sixes
+        ss = set.intersection(*sixes)
+        for c in {'b', 'g', 'a', 'f'}:
+            candidates[c] &= ss
 
-    candidates['e'] -= candidates['g']
-    candidates['c'] -= candidates['f']
+        # c,e = 8-5
+        ans[5] = candidates['a'] | candidates['b'] | candidates['d'] | candidates['f'] | candidates['g']
+        candidates['c'] &= ans[8]-ans[5]
+        candidates['e'] &= ans[8]-ans[5]
 
-    rev_candidates = {candidates[c].pop(): c for c in candidates}
+        # Map signal chars to proper chars
+        key = { next(iter(candidates[c])): c for c in candidates }
 
-    out = []
-    for o in outputs:
-        real = set()
-        for c in o:
-            real.add(rev_candidates[c])
-        final_outs.append(proper_inv[''.join(sorted(real))])
-        out.append(proper_inv[''.join(sorted(real))])
-    full_outs.append(int(''.join(map(str, out))))
+        # Decode the output
+        out = ''
+        for o in output.strip().split():
+            real = proper[''.join(sorted(map(key.get, o)))]
+            if real in ['1', '4', '7', '8']:
+                part1 += 1
+            out += real
+        part2 += int(out)
 
-print('Part 1:', len(list(filter(lambda n: n in [1,4,7,8], final_outs))))
+print('Part 1:', part1)
 
-print('Part 2:', sum(full_outs))
+print('Part 2:', part2)
