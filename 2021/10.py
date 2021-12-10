990d5b6d650264f8a3033be7683f5baf81960145
Max Meinhold <mxmeinhold@gmail.com>
2021-12-10 00:43:30 -0500

    [2021][10] Cleaned up a little

--- 2021/10.py
+++ 2021/10.py
@@ -1,73 +1,64 @@
 #! /bin/python3
 
 import sys
+from functools import reduce
 
-scores = []
-with open(sys.argv[1], 'r') as in_file:
-    lines = list(map(str.strip,in_file))
-
-    illegal = []
-    for line in lines:
-        stack = []
-        good= True
-        for c in line:
-            if c in '([{<':
-                stack.append(c)
-            elif c in '>]})':
-                if c == '>':
-                    if stack[-1] == '<':
-                        stack.pop()
-                    else:
-                        good=False
-                        illegal.append(c)
-                        break
-                if c == ']':
-                    if stack[-1] == '[':
-                        stack.pop()
-                    else:
-                        good=False
-                        illegal.append(c)
-                        break
-                if c == '}':
-                    if stack[-1] == '{':
-                        stack.pop()
-                    else:
-                        good=False
-                        illegal.append(c)
-                        break
-                if c == ')':
-                    if stack[-1] == '(':
-                        stack.pop()
-                    else:
-                        good=False
-                        illegal.append(c)
-                        break
-        if good:
-            fix = ''.join(stack).replace('(',')').replace('{','}').replace('[',']').replace('<','>')
-            lf = list(fix)
-            lf.reverse()
-            fix = ''.join(lf)
-            score = 0
-            p = {
-                ')': 1,
-                ']': 2,
-                '}': 3,
-                '>': 4
-            }
-            for c in fix:
-                score *= 5
-                score += p[c]
-            scores.append(score)
-
-
-points = {
+# Points for part 1
+part1 = {
     ')':3,
     ']':57,
     '}':1197,
     '>':25137
 } 
 
+# Points for part 2
+part2 = {
+    ')': 1,
+    ']': 2,
+    '}': 3,
+    '>': 4
+}
+
+# A convenience function for swapping pairs
+swap = {
+    '(': ')',
+    '{': '}',
+    '[': ']',
+    '<': '>',
+    ')': '(',
+    '}': '{',
+    ']': '[',
+    '>': '<',
+}.get
+
+scores = []
+
+with open(sys.argv[1], 'r') as in_file:
+    lines = list(map(str.strip,in_file))
+
+    corrupted = []
+    for line in lines:
+        stack = []
+        for c in line:
+            if c in '([{<':
+                stack.append(c)
+            elif c in '>]})':
+                if stack[-1] == swap(c):
+                    stack.pop()
+                else:
+                    corrupted.append(c)
+                    break
+        else:
+            scores.append(reduce(
+                lambda base, new: base * 5 + new,
+                map(part2.get,
+                    map(swap,
+                        reversed(stack)
+                    )
+                )
+            ))
+
 fin = sorted(scores)
-print('Part 1:', sum(map(points.get, illegal)))
+print('Part 1:', sum(map(part1.get, corrupted)))
 print('Part 2:', fin[int(len(fin)/2)])
 
