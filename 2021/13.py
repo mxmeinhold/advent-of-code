f73199905f289ee25860e8fe51d608148c7fc6d6
Max Meinhold <mxmeinhold@gmail.com>
2021-12-13 01:35:43 -0500

    [2021][13] Make 13.py use `map()` a lot more
    Apparently I like `map` a lot. Also lambdas.

--- 2021/13.py
+++ 2021/13.py
@@ -1,39 +1,50 @@
 #! /bin/python3
 
+from functools import reduce
 import sys
+import time
 
 with open(sys.argv[1], 'r') as in_file:
     a = ''.join(in_file).split('\n\n')
-    coords = set(map(lambda l: tuple(map(int, l.strip().split(','))), a[0].splitlines()))
-    instrs = list(map(str.strip, a[1].splitlines()))
+    dots = set(map(lambda l: tuple(map(int, l.split(','))), a[0].splitlines()))
+    instrs = map(lambda instr: instr.removeprefix('fold along '), a[1].splitlines())
+    instrs = map(lambda instr: instr.split('='), instrs)
+    instrs = map(lambda instr: (instr[0], int(instr[1])), instrs)
 
-for instr in instrs:
-    line, num = tuple(instr.removeprefix('fold along ').split('='))
-    num = int(num)
+# Filter out the dots on the fold line, since they get destroyed
+# axis is 'x' or 'y', the axis of the fold
+# line is the line (e.g. 5 for x=5) that the fold is on
+filt = lambda axis, line: lambda dot: dot[0 if axis == 'x' else 1] != line
 
-    new_coords = set()
-    if line == 'x':
-        for coord in coords:
-            if coord[0] > num:
-                new_coords.add( (num-(coord[0] - num), coord[1]))
-            elif coord[0] != num:
-                new_coords.add(coord)
-    elif line == 'y':
-        for coord in coords:
-            if coord[1] > num:
-                new_coords.add((coord[0], num - (coord[1]- num)))
-            elif coord[1] != num:
-                new_coords.add(coord)
-    coords = new_coords
-# I just commented these out to do part 2
-    #break
-#print(len(set(coords)))
+# Compute the new dot position after the fold
+# Doing this with abs means I can apply this function to every point, not just
+# the ones past the fold
+# axis is 'x' or 'y', the axis of the fold
+# line is the line (e.g. 5 for x=5) that the fold is on
+func = lambda axis, line: lambda dot: (line - abs(dot[0] - line), dot[1]) if axis == 'x' else (dot[0], line - abs(dot[1] - line))
 
-x = max(map(lambda c: c[0], coords))
-y = max(map(lambda c: c[1], coords))
-out = [['.' for i in range(x+1)] for j in range(y+1)]
+# Apply the instruction to dots
+apply = lambda dots, instr: set(map(func(*instr), filter(filt(*instr), dots)))
 
-for c in coords:
-    out[c[1]][c[0]] = '#'
 
-print('\n'.join(map(''.join, out)))
+# We need the number of dots left after the first fold for part 1
+start = time.time_ns()
+dots = apply(dots, next(instrs))
+end = time.time_ns()
+print('Part 1:', len(dots), f'({end-start} nanoseconds, or {(end-start)/1000000}ms)')
+
+# And process the rest of the instrs for part 2
+start = time.time_ns()
+dots = reduce(apply, instrs, dots)
+
+# Humans are faster at parsing ascii art text than I can write code, so let's
+# just print it. I'm using ' ' instead of '.' like in the input because it
+# makes it so much easier to read.
+print('\n'.join(map(''.join, ((
+        '#' if (x,y) in dots else ' '
+        for x in range(max(map(lambda d: d[0], dots))+1)
+    )
+    for y in range(max(map(lambda d: d[1], dots))+1)
+))))
+end = time.time_ns()
+print(end-start, f'nanoseconds, or {(end-start)/1000000}ms')
